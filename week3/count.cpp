5e3f20b46836d54af058d2263d8db639d8a046b8
Mahima Pandey <mahima23pandey@gmail.com>
2026-02-28 22:07:01 +0530

    count.cpp

---
+++ week3/count.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/
+void insertionSort(vector<int> &arr, long long &comparisons, long long &shifts)
+{
+    int n = arr.size();
+    comparisons = 0;
+    shifts = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0)
+        {
+            comparisons++;   
+
+            if (arr[j] > key)
+            {
+                arr[j + 1] = arr[j]; 
+                shifts++;
+                j--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int T;   
+    cin >> T;
+
+    while (T--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        long long comparisons, shifts;
+
+        insertionSort(arr, comparisons, shifts);
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        cout << "comparisons = " << comparisons << endl;
+        cout << "shifts = " << shifts << endl;
+    }
+
+    return 0;
+}
